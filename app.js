adb0697e560aefa98f165662732e080579d82e49
Gurkaran Singh <gurkaran@grevity.in>
2020-06-29 20:47:22 +0530

    minor changes

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const expressLayouts = require('express-ejs-layouts');
-const engine = require('ejs-locals');
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressLayouts = require("express-ejs-layouts");
+const engine = require("ejs-locals");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
-
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/assets', express.static('assets'))
-app.use('/',require('./routes/paths'));
+app.use("/assets", express.static("assets"));
+app.use("/", require("./routes/paths"));
 
-app.engine('ejs', engine);
+app.engine("ejs", engine);
 app.use(expressLayouts);
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
